7e2e5f528df4af04832c5eb12bbd74995d00c333
unknown <paulo.ferreira.pb@compasso.com.br>
2023-04-19 18:35:39 -0300

    fix: melhor avaliação do dado inserido pelo usuário.

--- scr/JS/script.js
+++ scr/JS/script.js
@@ -1,65 +1,76 @@
 let raioDeProximidade = 0.15; //Raio de proximidade entre o valor digitado e o pin sorteado
 let tentativa = 0; // Variável para armazenar o valor inserido
-let range = 999; // Tamanho do invervalo de dados possíveis
-let pin = getPin(range); // Pin a ser advinhado
-let repeticao = true;
+let numeroDeDigitos = 3; // Quantos dígitos deverá ter o PIN
+let pin = getPin(numeroDeDigitos); // Pin a ser advinhado
+let repeticao = true; // Controlador de repeticao da solicitação de pin
 
 alert("Avaliação de Scrum: Verificação de PIN.");
-tentativa = prompt("Insira um número de 0 a 999: ");
+tentativa = prompt( `Insira um PIN de ${numeroDeDigitos} dígitos: `);
 
 while(repeticao){
 
-    switch( avaliacaoDoDado(tentativa, pin, raioDeProximidade, range) ){
+    switch( avaliacaoDoDado(tentativa, pin, raioDeProximidade, numeroDeDigitos) ){
         case 1:
-            alert("Parabéns, você acertou!!!\nO pin era " + ("000" + pin).slice(-3));
+            alert("Parabéns, você acertou!!!\nO PIN era " + ("000" + pin).slice(-3));
             repeticao = false;
             break;
         case 2:
-            tentativa = prompt("Quase! O tente um valor maior: ");
+            tentativa = prompt("Quase! Tente um PIN MAIOR: ");
             break;
         case 3:
-            tentativa = prompt("Quase! O tente um valor menor: ");
+            tentativa = prompt("Quase! Tente um PIN MENOR: ");
             break;
         case 4:
-            tentativa = prompt("Errado... O tente um valor muito maior: ");
+            tentativa = prompt("Errado... Tente um PIN muito MAIOR: ");
             break;
         case 5:
-            tentativa = prompt("Errado... O tente um valor muito menor: ");
+            tentativa = prompt("Errado... Tente um PIN muito MENOR: ");
             break;
+        case 6:
+            tentativa = prompt(`Entrada inválida... Insira um PIN de ${numeroDeDigitos} dígitos: `);
+            break;
+        default:
+            alert("Erro desconhecido. Dados no console");
+            repeticao = false;
+
+            // DADOS PARA AJUDAR A ENCONTRAR OU REPLICAR O ERRO
+            console.log("PIN: " + pin);
+            console.log("tentativa: " + tentativa);
+            console.log("Tipo da tentativa: " + typeof tentativa);
+            console.log("Valor retornado da avaliacao do dado: " + avaliacaoDoDado(tentativa, pin, raioDeProximidade, numeroDeDigitos));
+            break;        
     }
 
 }
 
 
-
-
-
-
-
-
-
-function getPin(range){
-    return Math.round(Math.random()*range);
+function getPin(numeroDeDigitos){
+    return Math.round(Math.random()*Math.pow(10,numeroDeDigitos));
 }
 
 
-function avaliacaoDoDado(valor, pin, raio, range){
-    let resultado = 0;
+function avaliacaoDoDado(valor, pin, raioDeProximidade, numeroDeDigitos){
+
+    //BLOCO DE TESTES DE VALIDADE DO DADO
+    if((valor.length != numeroDeDigitos) || (valor*1 < 0) || (isNaN(valor*1)) || ((valor % 1) != 0)){
+        return 6; //Casos rejeitados: Valor vazio, quantiade de digitos diferente, numeros negativos, texto e numeros decimais
+    }
+
+    //BLOCO DE TESTE DE PROXIMIDADE
     if(valor == pin){
-
-        resultado = 1;
-
-    }else if(Math.abs(valor -  pin) <= (raio * range)){
+        return 1; // Retorno caso o usuário acerte
+    }
+    if(Math.abs(valor -  pin) <= (raioDeProximidade * ( Math.pow(10,numeroDeDigitos)-1 ))){
         if(valor < pin){
-            resultado = 2;
+            return 2; // Retorno caso a tentativa esteja próxima e abaixo do PIN
         }else{
-            resultado = 3;
+            return 3; // Retorno caso a tentativa esteja próxima e acima do PIN
         }
     }else{
         if(valor < pin){
-            resultado = 4;
+            return 4; // Retorno caso a tentativa esteja longe e abaixo do PIN
         }else{
-            resultado = 5;
+            return 5; // Retorno caso a tentativa esteja longe e acima do PIN
         }
     }
 
