cb9773bf897ddbac225abd5f10ebab133f560abb
Gabriel Couto de Freitas <gabriel.freitas.pb@compasso.com.br>
2023-04-20 09:20:15 -0300

    docs: Cógigo principal comentado e explicado.

--- src/script.js
+++ src/script.js
@@ -1,35 +1,38 @@
-let pin=5000;           //Valor do pin fixo para facilitar testes de funcionalidade    
-let acertou=false;                   
+let pin=getRandomPin(1000,9999);    //Gera um PIN aleatorio de 4 digitos
+let acertou=false;                  //Variavel bool para marcar quando o usuario acertou o PIN
 
-console.log(`Pin sorteado aleatoriamente: ${pin}`);                    
-                                     
+console.log(`Pin sorteado aleatoriamente: ${pin}`); /*
+                                                    Mostra no console do programador o PIN
+                                                    que foi gerado aleatoriamente, com fim 
+                                                    de testar as funcionalidades 
+                                                    */
 
-while(!acertou){
-    let guess=prompt("Insira o PIN:");
-    let diferenca=getDifference(pin,guess);
+while(!acertou){    //Realiza o loop enquanto o usuário não acertar o PIN
+    let guess=prompt("Insira o PIN:");              //Pede ao usuario uma tentativa para o PIN
+    let diferenca=getDifference(pin,guess);         //Armazena a diferença entre a tentativa e o PIN
 
-    if(diferenca==0){
+    if(diferenca==0){           //Se for 0 a diferença, o usuário acertou o pin
         alert(`Parabéns! Você acertou o PIN ${pin}`);
-        acertou=true;
+        acertou=true;           //Seta a condição de parada do loop 
     }
-    else if(diferenca>0){
-        if(diferenca>=1000)
+    else if(diferenca>0){       //Se a tentativa foi menor que o PIN:   
+        if(diferenca>=1000)     //Se foi 1000 ou mais números menor, foi "MUITO menor"
             alert(`Nem perto! ${guess} é MUITO menor que o PIN! Tente novamente.`);
-        else
+        else                    //Se não, foi "menor"
             alert(`Quase! ${guess} é menor que o PIN! Tente novamente`);
     }
-    else{
-        if(diferenca<=-1000)
+    else{                       //Se a tentativa foi maior que o PIN:
+        if(diferenca<=-1000)    //Se foi 1000 ou mais números maior, foi "MUITO maior"
             alert(`Nem perto! ${guess} é MUITO maior que o PIN! Tente novamente.`);
-        else
+        else                    //Se não, foi "menor"
             alert(`Quase! ${guess} é maior que o PIN! Tente novamente.`);
     }
 }
 
-function getDifference(pin,guess){
+function getDifference(pin,guess){  //Função que retorna o valor da diferença da tentativa e do PIN
     return pin-guess;
 }
 
-function getRandomPin(min, max){
+function getRandomPin(min, max){    //Gera um PIN aleatório variando do valor MIN ao valor MAX
     return Math.floor(Math.random()*(max-min+1)+min);
 }
