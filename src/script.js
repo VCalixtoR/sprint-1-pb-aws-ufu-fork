5bdfd97f2c8a2d309d956b89e5f8ed1b7a0d34b6
Gabriel Couto de Freitas <gabriel.freitas.pb@compasso.com.br>
2023-04-21 21:30:52 -0300

    fix: Corrigido o erro de ordem do prompt, html alterado para isso.

--- src/script.js
+++ src/script.js
@@ -1,49 +1,49 @@
-let pin=getRandomPin(1000,9999);                    //Gera um PIN aleatorio de 4 digitos
-let acertou=false;                                  //Variavel bool para marcar quando o usuario acertou o PIN
+function main(){
+    let pin=getRandomPin(1000,9999);                    //Gera um PIN aleatorio de 4 digitos
+    let acertou=false;                                  //Variavel bool para marcar quando o usuario acertou o PIN
 
-console.log(`Pin sorteado aleatoriamente: ${pin}`); /*
-                                                    Mostra no console do programador o PIN
-                                                    que foi gerado aleatoriamente, com fim 
-                                                    de testar as funcionalidades.
-                                                    Devido a ordem de prioridade das funcoes 
-                                                    prompt() e console.log(), as mensagens 
-                                                    do console aparecem apenas após o reload
-                                                    da página. 
-                                                    */
-                                                   
-while(!acertou){                                    //Realiza o loop enquanto o usuário não acertar o PIN
-    let guess=prompt("Insira o PIN de 4 dígitos:"); //Pede ao usuario uma tentativa para o PIN
-    guess=parseInt(guess);                          //Transforma a string inserida em number
+    console.log(`Pin sorteado aleatoriamente: ${pin}`); /*
+                                                        Mostra no console do programador o PIN
+                                                        que foi gerado aleatoriamente, com fim 
+                                                        de testar as funcionalidades.
+                                                        */
 
-    if(isNaN(guess)){                               //Se nao for inserido um número, guess é NaN
-        alert("Tente novamente, digite um número válido.");
-        continue;
+    while(!acertou){                                    //Realiza o loop enquanto o usuário não acertar o PIN
+        //debugger;   Usado na fase de testes, para facilitar os testes e monitorar variáveis
+        
+        let guess=prompt("Insira o PIN de 4 dígitos:"); //Pede ao usuario uma tentativa para o PIN
+        guess=parseInt(guess);                          //Transforma a string inserida em number
+
+        if(isNaN(guess)){                               //Se nao for inserido um número, guess é NaN
+            alert("Tente novamente, digite um número válido.");
+            continue;
+        }
+
+        let diferenca=getDifference(pin,guess);         //Armazena a diferença entre a tentativa e o PIN
+
+        if(diferenca==0){                               //Se for 0 a diferença, o usuário acertou o pin
+            alert(`Parabéns! Você acertou o PIN ${pin}`);
+            acertou=true;                               //Seta a condição de parada do loop 
+        }
+        else if(diferenca>0){                           //Se a tentativa foi menor que o PIN:   
+            if(diferenca>=1000)                         //Se foi 1000 ou mais números menor, foi "MUITO menor"
+                alert(`Nem perto! ${guess} é MUITO menor que o PIN! Tente novamente.`);
+            else                                        //Se não, foi "menor"
+                alert(`Quase! ${guess} é menor que o PIN! Tente novamente`);
+        }
+        else{                                           //Se a tentativa foi maior que o PIN:
+            if(diferenca<=-1000)                        //Se foi 1000 ou mais números maior, foi "MUITO maior"
+                alert(`Nem perto! ${guess} é MUITO maior que o PIN! Tente novamente.`);
+            else                                        //Se não, foi "menor"
+                alert(`Quase! ${guess} é maior que o PIN! Tente novamente.`);
+        }
     }
 
-    let diferenca=getDifference(pin,guess);         //Armazena a diferença entre a tentativa e o PIN
-
-    if(diferenca==0){                               //Se for 0 a diferença, o usuário acertou o pin
-        alert(`Parabéns! Você acertou o PIN ${pin}`);
-        acertou=true;                               //Seta a condição de parada do loop 
+    function getDifference(pin,guess){                  //Função que retorna o valor da diferença da tentativa e do PIN
+        return pin-guess;
     }
-    else if(diferenca>0){                           //Se a tentativa foi menor que o PIN:   
-        if(diferenca>=1000)                         //Se foi 1000 ou mais números menor, foi "MUITO menor"
-            alert(`Nem perto! ${guess} é MUITO menor que o PIN! Tente novamente.`);
-        else                                        //Se não, foi "menor"
-            alert(`Quase! ${guess} é menor que o PIN! Tente novamente`);
-    }
-    else{                                           //Se a tentativa foi maior que o PIN:
-        if(diferenca<=-1000)                        //Se foi 1000 ou mais números maior, foi "MUITO maior"
-            alert(`Nem perto! ${guess} é MUITO maior que o PIN! Tente novamente.`);
-        else                                        //Se não, foi "menor"
-            alert(`Quase! ${guess} é maior que o PIN! Tente novamente.`);
-    }
-}
 
-function getDifference(pin,guess){                  //Função que retorna o valor da diferença da tentativa e do PIN
-    return pin-guess;
-}
-
-function getRandomPin(min, max){                    //Gera um PIN aleatório variando do valor MIN ao valor MAX
-    return Math.floor(Math.random()*(max-min+1)+min);
+    function getRandomPin(min, max){                    //Gera um PIN aleatório variando do valor MIN ao valor MAX
+        return Math.floor(Math.random()*(max-min+1)+min);
+    }
 }
